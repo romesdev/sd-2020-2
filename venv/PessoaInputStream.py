0316b1004d68c2f78b14b6b7d5d2573c78635125
romesdev <fs.romes@gmail.com>
2021-02-09 11:14:41 -0300

    almost 1

--- venv/PessoaInputStream.py
+++ venv/PessoaInputStream.py
@@ -1,10 +1,20 @@
 from Pessoa import *
-import sys
+from client import *
+import sys, pickle
 
 class PessoaInputStream:
     def __init__(self, origem):
         self.origem = origem
 
+    def __read__(self):
+        texto = ""
+        file = open(self.origem, "rb")
+        pessoas = pickle.load(file)
+        qtd = len(pessoas)
+        for p in pessoas:
+            print("Bytes do Objeto: {}".format(sys.getsizeof(p)))
+            client(message=p, size=qtd)
+            qtd = qtd - 1
 
 p1 = Pessoa("Joao Joao", "67308573922", 32)
 
@@ -14,3 +24,6 @@ lista.append(p1)
 lista.append(p1)
 
 print(lista)
+
+pIn = PessoaInputStream("pessoas.txt")
+pIn.__read__()
